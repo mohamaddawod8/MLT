895df81f95b4e103b2d89c967566a7429a101e51
Ranim99582 <Rannom.ab.99@gmail.com>
2026-01-13 20:58:12 +0300

    Ranim_326483

--- src/journey_analysis.py
+++ src/journey_analysis.py
@@ -11,5 +11,5 @@ def run_analysis(df):
     journeys.columns = ['Account_ID', 'Country', 'Solution', 'Outcome', 'Full_Path']
     
     top_5 = journeys.groupby(['Country', 'Solution'])['Full_Path'].value_counts().groupby(level=[0,1]).head(5)
-    top_5.to_csv('../Data/top_5_chronological_paths.csv')
+
     return journeys
